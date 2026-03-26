2dd9a56a9245539667d0336dc4de5e014fe87658
wa <wa@agiteks.com>
2026-03-27 00:41:59 +0500

    chore: Add console log for accessToken in user action functions.

--- client/src/actions/user.action.js
+++ client/src/actions/user.action.js
@@ -5,6 +5,7 @@ const base_url = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const getAllUsers = async () => {
     const cookieStore = await cookies()
     const accessToken = cookieStore.get("accessToken")?.value
+    console.log(accessToken);
     try {
         const res = await fetch(`${base_url}/users`, {
             method: 'GET',
@@ -27,6 +28,7 @@ export const getAllUsers = async () => {
 export const getUserById = async (id) => {
     const cookieStore = await cookies()
     const accessToken = cookieStore.get("accessToken")?.value
+    console.log(accessToken);
     try {
         const res = await fetch(`${base_url}/users/${id}`, {
             method: 'GET',

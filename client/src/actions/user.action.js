fab7af2af4ecfefabd06872e6014cb45f50e5a6f
wa <wa@agiteks.com>
2026-03-27 12:30:33 +0500

    try: made-user-csr

--- client/src/actions/user.action.js
+++ client/src/actions/user.action.js
@@ -1,18 +1,11 @@
-import { getAuthHeaders } from "@/constants/authHeaders";
+"use client";
+import api from "@/api/api";
 
 const base_url = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const getAllUsers = async () => {
     try {
-        const headers = await getAuthHeaders();
-        console.log(headers);
-        const res = await fetch(`${base_url}/users`, {
-            method: 'GET',
-            headers,
-            cache: "no-store"
-        });
-        console.log(res);
-        const data = await res.json()
+        const { data } = await api.get(`/users`);
         return data
     } catch (error) {
         console.log(error)
@@ -22,15 +15,7 @@ export const getAllUsers = async () => {
 
 export const getUserById = async (id) => {
     try {
-        const headers = await getAuthHeaders();
-        console.log(headers);
-        const res = await fetch(`${base_url}/users/${id}`, {
-            method: 'GET',
-            headers,
-            cache: "no-store"
-        });
-        console.log(res);
-        const data = await res.json()
+        const { data } = await api.get(`/users/${id}`);
         return data
     } catch (error) {
         console.log(error)

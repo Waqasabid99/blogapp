0d97b7088d3be5075696664e57a98cf338ddc496
wa <wa@agiteks.com>
2026-03-31 02:11:39 +0500

    testing error

--- client/src/app/test-error/page.jsx
+++ client/src/app/test-error/page.jsx
@@ -1,3 +1,6 @@
-export default function TestError() {
-    throw new Error("Global error test");
+export const dynamic = "force-dynamic";
+
+export default async function TestError() {
+    await new Promise(res => setTimeout(res, 100));
+    throw new Error("Runtime error");
   }
